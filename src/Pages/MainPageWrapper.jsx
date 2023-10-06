2e272653926f507acf58986db9822c5d057876dc
emanson1 <edwardmaddenanson@gmail.com>
2023-10-06 10:45:35 -0500

    link placement

--- src/Pages/MainPageWrapper.jsx
+++ src/Pages/MainPageWrapper.jsx
@@ -268,12 +268,12 @@ const MainPageWrapper = props => {
       <div className={classes.logoClass}><img src={CofCLogo} style={{width:'60%'}}/></div></Grid>
       <Grid item xs={8} sm={6}>
       <Grid container className={classes.linkOffset}>
-      <Grid item xs={2} className={classes.linkClass} style={{textAlign:'center'}} ><a onClick={()=>setCurWindow(<Home/>)}>Home</a></Grid>
-      <Grid item xs={2} className={classes.linkClass} style={{textAlign:'right'}} ><a onClick={()=>setCurWindow(<Services/>)}>Services</a></Grid>
+      <Grid item xs={2} className={classes.linkClass} style={{textAlign:'left'}} ><a onClick={()=>setCurWindow(<Home/>)}>Home</a></Grid>
+      <Grid item xs={3} className={classes.linkClass} style={{textAlign:'center'}} ><a onClick={()=>setCurWindow(<Services/>)}>Services</a></Grid>
       <Grid item xs={2} className={classes.linkClass} style={{textAlign:'right'}} ><a onClick={()=>setCurWindow(<Gallery/>)}>Gallery</a></Grid>
       {/* <Grid item xs={2} className={classes.linkClass} ><a onClick={()=>setCurWindow(<About/>)}>About</a></Grid> */}
-      <Grid item xs={3} className={classes.linkClass} ><a href="https://classic-floors-of-charleston.business.site/?utm_source=gmb&utm_medium=referral" target="_blank">Google</a></Grid>
-      <Grid item xs={3} className={classes.linkClass} ><a href="https://www.facebook.com/people/CFC-Hardwood-Floors-LLC/100067691010274/" target="_blank">Facebook</a></Grid>
+      <Grid item xs={2} className={classes.linkClass}  style={{textAlign:'center'}}><a href="https://classic-floors-of-charleston.business.site/?utm_source=gmb&utm_medium=referral" target="_blank">Google</a></Grid>
+      <Grid item xs={3} className={classes.linkClass}  style={{textAlign:'right'}}><a href="https://www.facebook.com/people/CFC-Hardwood-Floors-LLC/100067691010274/" target="_blank">Facebook</a></Grid>
       </Grid>
       <Grid container>
       <Grid item xs={12}><Typography variant={'h2'} className={classes.TitleClass} style={{textAlign:'center'}}> CFC Hardwood Floors LLC</Typography></Grid>
