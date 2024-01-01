37c7befaec8c12174994f6e26acd363da8c541ad
emanson1 <edwardmaddenanson@gmail.com>
2024-01-01 18:04:45 -0500

    testing env

--- src/Modals/QuoteModal.jsx
+++ src/Modals/QuoteModal.jsx
@@ -22,8 +22,6 @@ import {
 const BUILD_ENV = process.env.REACT_APP_BUILD_ENV || "any-default-local-build_env";
 const emailHtml = render(<Email url="https://www.cfchardwoodfloorsllc.com" />);
 
-
-
 export default function QuoteModal(props) {
   const [imageList, setImageList] = useState([]);
   const [image64, setImage64] = useState({});
