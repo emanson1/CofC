a41beb0c3af73d5f0bbdce9bd00c93f1485bb4cb
emanson1 <edwardmaddenanson@gmail.com>
2024-01-28 13:22:30 -0500

    fixed open window function

--- src/Pages/Navigation.jsx
+++ src/Pages/Navigation.jsx
@@ -9,12 +9,13 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import CofCLogoSmall from '../Images/CFCLogoSmall.png';
 import { showModal, hideModal } from '../actions/bluevilleActions';
 import { connect } from 'react-redux';
+import { blue } from '@material-ui/core/colors';
 const useStyles = makeStyles((theme) => ({
     iconSize: {
         color: '#003569',
         transform: 'scale(11.0)',
-        paddingTop: 5,
-        paddingLeft: 10,
+        paddingRight: 20,
+        paddingLeft: 20,
         transform: 'scale(2.0)',
     },
     iconGrid: {
@@ -26,120 +27,144 @@ const useStyles = makeStyles((theme) => ({
         fontWeight: 'bold',
         textShadow: '-1px 0 #8C92B4, 0 3px #8C92B4, 1px 0 #8C92B4, 0 -1px #8C92B4',
         paddingTop: 15,
-        fontSize: 24,
+        fontSize: 30,
     },
     subContainerBackground: {
         backgroundImage: `url(${CofCLogoSmall})`,
         backgroundSize: 'cover',
-      },
-      subContainerOpacity:{
-backgroundColor:'white',
-opacity:'90%'
-      },
-     navClass:{
-        minHeight:500
-     },
-quoteButton:{
-    fontWeight:'bold',
-    minWidth:'100%',
-    textAlign:'center',
-    backgroundColor:'yellow',
-},
-quoteButtonContainer:{
-    width:'100%',
-    textAlign:'center',
-}
+    },
+    subContainerOpacity: {
+        backgroundColor: 'white',
+        opacity: '90%',
+            },
+    navClass: {
+        minHeight: 500
+    },
+    quoteButton: {
+        fontWeight: 'bold',
+        minWidth: '100%',
+        textAlign: 'center',
+        backgroundColor: 'yellow',
+    },
+    quoteButtonContainer: {
+        minWidth: 200,
+        textAlign: 'center',
+        padding: 20
+    },
+    linkClass: {
+        textDecoration: 'none',
+        fontWeight: 'bold',
+        color: '#003569',
+        fontWeight: 'bold',
+        textShadow: '-1px 0 blue, 0 3px yellow, 1px 0 #8C92B4, 0 -1px #8C92B4',
+        fontSize: 24
+    },
+    navClass:{
+        border: '5px solid #8C92B4',
+        borderRadius: 5
+
+    }
 }))
 const Navigation = (props) => {
     const { open, setOpen, setCurWindow, Home, Services, Gallery } = props;
     const classes = useStyles();
     var openModal = (modalProps) => {
         props.showModal(modalProps);
-      }
+    }
+    const openWindow = (curWin) =>
+    {
+        setOpen(false);
+        setCurWindow(curWin);
+    }
     const top =
-        
-            <Grid container className={classes.navClass}>
-                <Grid item xs={12}>
-                    <Typography variant={'h2'} className={classes.TitleClass}>
-                        CFC Hardwood Floors LLC
-                    </Typography>
-                </Grid>
-                <Grid container className={classes.subContainerBackground}>
+
+        <Grid container className={classes.navClass}>
+            <Grid item xs={12}>
+                <Typography variant={'h2'} className={classes.TitleClass}>
+                    CFC Hardwood Floors LLC<hr/>
+                </Typography>
+            </Grid>
+            <Grid container className={classes.subContainerBackground}>
                 <Grid item xs={12}>
                     <Grid container className={classes.subContainerOpacity}>
-                <Grid item xs={12}>
-                    <a href="#" onClick={() => setCurWindow(Home)}>
-                        <Grid container>
-                            <Grid className={classes.iconGrid} item xs={2}>
-                                <HomeIcon className={classes.iconSize} />
-                            </Grid>
-                            <Grid item xs={10}>
-                                Home
+                        <Grid item xs={12}>
+                        <Grid item xs={12}>
+                            <Grid container className={classes.quoteButtonContainer}>
+                               <br/>
                             </Grid>
                         </Grid>
-                    </a>
-                </Grid>
-                <Grid item xs={12}>
-                    <a href="#" onClick={() => setCurWindow(Services)}>
-                        <Grid container>
-                            <Grid className={classes.iconGrid} item xs={2}>
-                                <EngineeringIcon className={classes.iconSize} />
-                            </Grid>
-                            <Grid item xs={10}>
-                                Services
-                            </Grid>
+                            <a className={classes.linkClass} href="#" onClick={() => openWindow(Home)}>
+                                <Grid container>
+                                    <Grid className={classes.iconGrid} item xs={2}>
+                                        <HomeIcon className={classes.iconSize} />
+                                    </Grid>
+                                    <Grid item xs={10}>
+                                        Home
+                                    </Grid>
+                                </Grid>
+                            </a>
                         </Grid>
-                    </a>
-                </Grid>
-                <Grid item xs={12}>
-                    <a href="#" onClick={() => setCurWindow(Gallery)}>
-                        <Grid container>
-                            <Grid className={classes.iconGrid} item xs={2}>
-                                <CameraIcon className={classes.iconSize} />
-                            </Grid>
-                            <Grid item xs={10}>
-                                Gallery
-                            </Grid>
+                        <Grid item xs={12}>
+                            <a className={classes.linkClass} href="#" onClick={() => openWindow(Services)}>
+                                <Grid container>
+                                    <Grid className={classes.iconGrid} item xs={2}>
+                                        <EngineeringIcon className={classes.iconSize} />
+                                    </Grid>
+                                    <Grid item xs={10}>
+                                        Services
+                                    </Grid>
+                                </Grid>
+                            </a>
                         </Grid>
-                    </a>
-                </Grid>
-                <Grid item xs={12}>
-                    <a href="https://classic-floors-of-charleston.business.site/?utm_source=gmb&utm_medium=referral" target="_blank">
-                        <Grid container>
+                        <Grid item xs={12}>
+                            <a className={classes.linkClass} href="#" onClick={() => openWindow(Gallery)}>
+                                <Grid container>
+                                    <Grid className={classes.iconGrid} item xs={2}>
+                                        <CameraIcon className={classes.iconSize} />
+                                    </Grid>
+                                    <Grid item xs={10}>
+                                        Gallery
+                                    </Grid>
+                                </Grid>
+                            </a>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <a className={classes.linkClass} href="https://classic-floors-of-charleston.business.site/?utm_source=gmb&utm_medium=referral" target="_blank">
+                                <Grid container>
 
-                            <Grid className={classes.iconGrid} item xs={2}>
-                                <GoogleIcon className={classes.iconSize} />
-                            </Grid>
-                            <Grid item xs={10}>
-                                Google Business Site
+                                    <Grid className={classes.iconGrid} item xs={2}>
+                                        <GoogleIcon className={classes.iconSize} />
+                                    </Grid>
+                                    <Grid item xs={10}>
+                                        Google Business Site
+                                    </Grid>
+                                </Grid>
+                            </a>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <a className={classes.linkClass} href="https://www.facebook.com/people/CFC-Hardwood-Floors-LLC/100067691010274/" target="_blank">
+                                <Grid container>
+                                    <Grid className={classes.iconGrid} item xs={2}>
+                                        <FacebookIcon className={classes.iconSize} />
+                                    </Grid>
+                                    <Grid item xs={10}>
+                                        Facebook
+                                    </Grid>
+                                </Grid>
+                            </a>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Grid container className={classes.quoteButtonContainer}>
+                                <a onClick={() => openModal({ open: true, modalType: 'quoteModal', data: {} })}>
+                                    <Button className={classes.quoteButton} variant="contained" >Request Quote</Button>
+                                </a>
                             </Grid>
                         </Grid>
-                    </a>
+                    </Grid>
+
                 </Grid>
-                <Grid item xs={12}>
-                    <a href="https://www.facebook.com/people/CFC-Hardwood-Floors-LLC/100067691010274/" target="_blank">
-                        <Grid container>
-                            <Grid className={classes.iconGrid} item xs={2}>
-                                <FacebookIcon className={classes.iconSize} />
-                            </Grid>
-                            <Grid item xs={10}>
-                                Facebook
-                            </Grid>
-                        </Grid>
-                    </a>
-                </Grid>
-                <Grid item xs={12}>
-                <Grid container className={classes.quoteButtonContainer}>
-                      <a onClick={() => openModal({ open: true, modalType: 'quoteModal', data: {} })}>
-                    <Button className={classes.quoteButton} variant="contained" >Request Quote</Button>
-                </a>
-                </Grid>
-                </Grid>
-                </Grid>
-                
-                </Grid>
-                </Grid>
-                </Grid>
+            </Grid>
+        </Grid>
     return (
         <React.Fragment>
             <React.Fragment key={'top'}>
@@ -156,10 +181,10 @@ const Navigation = (props) => {
 };
 const mapStateToProps = (state) => ({
     modalProps: state.blueville.modalProps
-  });
-  
-  const mapDispatchToProps = (dispatch) => ({
+});
+
+const mapDispatchToProps = (dispatch) => ({
     showModal: (modalProps, modalTypes) => dispatch(showModal(modalProps, modalTypes)),
     hideModal: () => dispatch(hideModal()),
-  });
-  export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
+});
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
