423176e23bc2a133a8f8411f8e1c108cc9ac687d
emanson1 <edwardmaddenanson@gmail.com>
2026-01-19 14:14:46 -0500

    added modal back removed aws library

--- src/Modals/QuoteModal.jsx
+++ src/Modals/QuoteModal.jsx
@@ -6,16 +6,16 @@ import { Grid, Box, Typography, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import CofCLogoSmall from '../Images/CFCLogoSmall.png';
 import Attachments from '../Pages/Attachments.jsx';
-import { SES } from '@aws-sdk/client-ses';
-import { AWS } from 'aws-sdk';
+//import { SES } from '@aws-sdk/client-ses';
+//import { AWS } from 'aws-sdk';
 //import { Email } from '../Pages/Email.jsx';
 //import { render } from '@react-email/render';
 import nodemailer from 'nodemailer';
-import {
-  SESClient,
-  SendRawEmailCommand,
-  SendEmailCommand,
-} from '@aws-sdk/client-ses';
+// import {
+//   SESClient,
+//   SendRawEmailCommand,
+//   SendEmailCommand,
+// } from '@aws-sdk/client-ses';
 
 
  
