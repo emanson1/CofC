d661c479abe490c48b1b39f5f9ba12bd5cf42ecd
emanson1 <edwardmaddenanson@gmail.com>
2026-02-28 14:25:16 -0500

    error email

--- src/Modals/QuoteModal.jsx
+++ src/Modals/QuoteModal.jsx
@@ -148,7 +148,7 @@ export default function QuoteModal(props) {
         e.target.reset(); // Optional: reset the form after success
       },
       (error) => {
-        console.log('FAILED...'+ error );
+        console.log('FAILED...', error);
         alert('Failed to send message. Please try again.');
       }
     );
